2a4a67ee335d5cc05982a301073961034b6592fc
ybin0423 <97583199+ybin0423@users.noreply.github.com>
2022-08-11 01:01:51 -0600

    Update student_mh.sql

--- student_mh.sql
+++ student_mh.sql
@@ -29,3 +29,29 @@ INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxi
 INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Male',19,'Engineering',1,'3.00-3.49','No','No','No','No');
 INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',18,'KENMS',2,'3.50-4.00','No','No','Yes','No');
 INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Male',24,'BCS',3,'3.50-4.00','No','No','No','No');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',24,'Accounting',3,'3.00-3.49','No','No','No','No');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',24,'ENM',4,'3.00-3.49','Yes','Yes','Yes','Yes');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',20,'BIT',2,'3.50-4.00','No','No','Yes','No');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',18,'Marine Science',2,'3.50-4.00','Yes','Yes','Yes','Yes');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',19,'Engineering',1,'3.00-3.49','No','No','No','Yes');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',18,'KOE',2,'3.00-3.49','No','Yes','No','No');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',24,'BCS',1,'3.50-4.00','No','No','No','No');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',24,'Engineering',1,'3.00-3.49','No','No','No','No');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',23,'BCS',3,'3.50-4.00','No','Yes','Yes','Yes');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',18,'Banking Studies',1,'3.50-4.00','No','No','No','No');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',19,'Engineering',1,'3.50-4.00','No','No','No','No');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Male',18,'Engineering',2,'3.00-3.49','Yes','Yes','Yes','No');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',24,'BIT',3,'3.50-4.00','Yes','Yes','Yes','Yes');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',24,'BCS',4,'3.50-4.00','No','No','No','No');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',23,'Business Administration',2,'3.00-3.49','No','No','No','No');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Male',18,'BCS',2,'3.00-3.49','No','No','No','No');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Male',19,'BCS',1,'3.50-4.00','No','No','No','Yes');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Male',18,'BCS',2,'3.50-4.00','Yes','Yes','Yes','No');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',19,'BIT',1,'3.00-3.49','No','Yes','Yes','Yes');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',18,'Engineering',1,'2.00-2.49','No','No','No','No');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',18,'Law',3,'3.00-3.49','No','Yes','Yes','No');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',19,'BIT',1,'2.50-2.99','No','Yes','Yes','Yes');
+INSERT INTO student_mh(gender,age,major,year,CGPA,marital_status,depression,anxiety,panic_attack) VALUES ('Female',18,'KIRKHS',1,'3.50-4.00','No','No','No','No');
+
+
+
