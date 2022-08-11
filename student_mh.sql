994aae4f89245465d94fa80b7fb2f924a318684f
ybin0423 <97583199+ybin0423@users.noreply.github.com>
2022-08-11 00:06:51 -0600

    Create student_mh.sql

---
+++ student_mh.sql
@@ -0,0 +1,15 @@
+/* Source From: 
+https://www.kaggle.com/datasets/osmi/mental-health-in-tech-survey
+*/
+CREATE TABLE student_mh (
+id INTEGER PRIMARY KEY,
+gender TEXT,
+age INTEGER,
+major TEXT,
+year INTEGER,
+CGPA REAL, 
+marital_status TEXT,
+depression TEXT,
+anxiety TEXT,
+panic_attack TEXT
+);
