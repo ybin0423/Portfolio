ed4c56931ef4f383c11d55a66280864620404204
ybin0423 <97583199+ybin0423@users.noreply.github.com>
2022-08-14 00:37:07 -0600

    Add files via upload

---
+++ simple chatbot/chatbot_momo.py
@@ -0,0 +1,56 @@
+#example / keyword / answer sections in excel
+#https://www.geeksforgeeks.org/python-get-key-from-value-in-dictionary/
+
+import pandas
+
+data = pandas.read_excel('Documents/chatbot_conversation.xlsx')
+
+momo = {}
+start = 0
+beg = 0
+m_dict = {}
+check_list=[]
+
+for r in data['keyword']:
+    momo[start] = r.split('/')
+    start += 1
+    
+for a in data['answer']:
+    m_dict[beg] = a
+    beg += 1
+
+def conversation(ask):
+    for key,value in momo.items():
+        check = 0
+        for v in value:
+            l = len(value)
+            if v in ask:
+                check += 1
+                if check == l:
+                    ans = m_dict[key]
+                    return ans
+    if check != l:
+        print('momo :( >> I do not understand, try again')
+                    
+                
+            
+
+while True:
+    print('-------------------------------')
+    print('Try a conversation with momo :D')
+    print('Press q to quit conversation')
+    a = input('You >>')
+    a = a.lower()
+    if a == 'q':
+        print('momo :D >> See you again.')
+        break
+    if a!= 'q'and (conversation(a) != None):
+        print('momo :D >> ',conversation(a))
+    
+  
+            
+    
+             
+        
+
+
